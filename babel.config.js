49ca11f4102ce28e7fd40175194a62ecd73e71f5
Serhii <avatarcapa@gmail.com>
2022-08-05 20:35:46 +0200

    Included 'React' import to fix dev. build - Fixed dev. build (accidentally pass a plugin as a preset)

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
-    '@babel/plugin-syntax-jsx',
     '@babel/preset-react',
   ],
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import type { AppProps } from 'next/app';
 
 import { wrapper } from '../redux/store';

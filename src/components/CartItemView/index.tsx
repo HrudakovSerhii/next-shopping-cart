a1c27fd21d3f3326c6bb0e0d602daea756689b95
Serhii <avatarcapa@gmail.com>
2022-08-05 12:06:32 +0200

    Updated CartItemView layout to fit 'shopping-cart-item' template

--- src/components/CartItemView/index.tsx
+++ src/components/CartItemView/index.tsx
@@ -5,8 +5,8 @@ import { CartItem } from '../../types';
 type CartItemAction = (gtin: string) => void;
 
 type CartItemProps = CartItem & {
-    incrementQuantity?: CartItemAction,
-    decrementQuantity?: CartItemAction,
+    incrementQuantity: CartItemAction,
+    decrementQuantity: CartItemAction,
     removeItem?: CartItemAction,
 }
 
@@ -30,43 +30,57 @@ const CartItemView: React.FC<CartItemProps> = ({
   };
 
   return (
-    <div className="cart-item-view">
-      <div className="image">
-        <img alt={name} src={imageUrl} />
+    <li className="cart-item-view flex py-4 w-full">
+      <div className="cart-item-image p-4 h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+        <img alt={name} src={imageUrl} className="h-full w-full object-cover object-center" />
       </div>
-      <div className="content">
-        <h3 className="name">{name}</h3>
-        <h4 className="price">
-          {recommendedRetailPrice
-            ? `${recommendedRetailPriceCurrency} ${recommendedRetailPrice}`
-            : 'No Price Available' }
-        </h4>
+
+      <div className="cart-item-content ml-4 flex flex-1 flex-col justify-between">
+        <div>
+          <div className="cart-item-name flex justify-between text-base font-medium text-gray-900">
+            <h3>
+              {name}
+            </h3>
+
+            <p className="cart-item-price ml-4">
+              {recommendedRetailPrice
+                ? `${recommendedRetailPriceCurrency} ${recommendedRetailPrice}`
+                : 'No Price Available' }
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center justify-between text-sm">
+          <p className="text-gray-500">{`Qty ${quantity}`}</p>
+          <div className="cart-item-controls flex flex-row items-center border-2">
+            <button
+              type="button"
+              disabled={quantity <= 1}
+              onClick={() => decrementQuantity(gtin)}
+              className="increment-quantity-btn px-3 py-1 font-medium text-indigo-600 hover:text-indigo-500 disabled:bg-aliceblue-300 disabled:opacity-30"
+            >
+              -
+            </button>
+            <h3 className="quantity mx-2">{quantity}</h3>
+            <button
+              type="button"
+              onClick={() => incrementQuantity(gtin)}
+              className="decrement-quantity-btn px-3 py-1 font-medium text-indigo-600 hover:text-indigo-500 disabled:bg-aliceblue-300 disabled:opacity-30"
+            >
+              +
+            </button>
+          </div>
+          <div className="flex">
+            <button
+              type="button"
+              className="remove-item-btn font-medium text-indigo-600 hover:text-indigo-500"
+              onClick={onRemoveItemClick}
+            >
+              Remove
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="controls">
-        <button
-          type="button"
-          onClick={() => incrementQuantity()}
-          className="increment-quantity-btn"
-        >
-          +
-        </button>
-        <h3 className="quantity">{quantity}</h3>
-        <button
-          type="button"
-          onClick={() => decrementQuantity()}
-          className="decrement-quantity-btn"
-        >
-          -
-        </button>
-      </div>
-      <button
-        type="button"
-        onClick={onRemoveItemClick}
-        className="remove-item-btn"
-      >
-        Add to Cart
-      </button>
-    </div>
+    </li>
   );
 };
 

5b6e7d24fdc18799232aed05bb6c5424ac46f924
Serhii <avatarcapa@gmail.com>
2022-08-05 20:24:47 +0200

    Fixed ESLint warning (hook rule)

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -14,7 +14,7 @@ const Checkout: React.FC = () => {
 
   useEffect(() => {
     dispatch(clearCart());
-  }, [state]);
+  }, [state, dispatch]);
 
   return (
     <div className="container mx-auto flex flex-col items-center justify-center h-screen">

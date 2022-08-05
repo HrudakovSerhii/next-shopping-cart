bed3d6b8c2a33a5b8ee1209f34e755d709de498e
Serhii <avatarcapa@gmail.com>
2022-08-05 12:24:36 +0200

    Introduced 'Image' component to cover broken img href condition - Included fallback.png image to cover broken img href

---
+++ src/components/Image/index.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+type ImageProps = {
+    src: string;
+    fallbackSrc: string;
+    alt: string;
+    className: string
+}
+
+const Image: React.FC<ImageProps> = ({
+  alt, src, fallbackSrc = '/fallback.png', ...rest
+}) => {
+  const [imgSrc, setImgSrc] = useState(src);
+
+  return (
+    <img
+      {...rest}
+      alt={alt}
+      src={imgSrc}
+      onError={() => {
+        setImgSrc(fallbackSrc);
+      }}
+    />
+  );
+};
+
+export default Image;
